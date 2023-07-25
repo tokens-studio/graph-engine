e360b7cb8ea20a681d0daf867c19816f844b036c
popdrazvan <popdrazvan@gmail.com>
2023-07-25 15:53:38 +0300

    fix configure store

--- packages/graph-editor/src/redux/index.tsx
+++ packages/graph-editor/src/redux/index.tsx
@@ -1,15 +1,7 @@
 import { Provider } from 'react-redux';
 import React from 'react';
-
-import { configureStore } from './store.tsx';
-import { EdgeType } from './models/settings.ts';
+import { store } from './store.tsx';
 
 export const ReduxProvider = ({ children }) => {
-  const store = configureStore({
-    settings: {
-      edgeType: EdgeType.smoothStep,
-    },
-  });
-
   return <Provider store={store}>{children}</Provider>;
 };
