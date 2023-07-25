add3953953421d592d9b675cb3d8480eaf25e25b
popdrazvan <popdrazvan@gmail.com>
2023-07-25 12:47:43 +0300

    fix types

--- packages/graph-editor/rollup.config.mjs
+++ packages/graph-editor/rollup.config.mjs
@@ -3,39 +3,27 @@ import {nodeResolve} from '@rollup/plugin-node-resolve';
 import tsPlugin from '@rollup/plugin-typescript';
 
 const defaultEntries = [
-	{
-		input: './src/index.tsx',
-		output: [
-			{
-				preserveModules: true,
-				dir: './dist/cjs',
-				exports: 'named',
-				sourcemap: true,
-				format: 'cjs'
-			},
-			{
-				preserveModules: true,
-				dir: './dist/esm',
-				exports: 'named',
-				sourcemap: true,
-				format: 'es'
-			}
-		],
-		plugins: [			
-			tsPlugin({
-				tsconfig: 'tsconfig.json'
-			}),
-			postcss({
-				extract: true
-			}),
-			// nodeResolve({
-			// 	alias: {
-			// 	  '#': __dirname + '/src',
-			// 	},
-			//   }),
-		],
-		cache: false
-	}
+    {
+        input: './src/index.tsx',
+        output: [
+            {
+                preserveModules: true,
+                dir: './dist',
+                exports: 'named',
+                sourcemap: true,
+                format: 'es'
+            }
+        ],
+        plugins: [			
+            tsPlugin({
+                tsconfig: 'tsconfig.json'
+            }),
+            postcss({
+                extract: true
+            }),
+        ],
+        cache: false
+    }
 ];
 
 export default defaultEntries;
