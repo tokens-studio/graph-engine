f0775330f78bddfc3e0f59bd42e04db2b25363c5
Neeraj Sinha <Neel_sinha@outlook.com>
2023-08-04 14:04:55 +0530

    Fix/tokenset validation (#53)
    * fix: validation failing for token set input
    
    * test: added test case for tokenset as an input
    
    * changeset: added changeset for the version bump
    
    * Create chatty-badgers-mix.md
    
    * cleanup: removed console
    
    * cleanup: removed unused changeset
    
    ---------
    
    Co-authored-by: Neeraj4.Sinha <neeraj4.sinha@ril.com>

--- packages/graph-engine/src/nodes/generic/input.ts
+++ packages/graph-engine/src/nodes/generic/input.ts
@@ -59,8 +59,11 @@ const validateInputs = (inputs, state) => {
     if (typeof definition == "object") {
       switch (definition.type) {
         case "tokenSet":
-          if (typeof inputs[key] !== "object") {
-            throw new Error("Expected object for input: " + key);
+          {
+            if (typeof inputs[key] !== "object") {
+              throw new Error("Expected object for input: " + key);
+            }
+            break;
           }
         case "string":
           if (typeof inputs[key] !== "string") {
