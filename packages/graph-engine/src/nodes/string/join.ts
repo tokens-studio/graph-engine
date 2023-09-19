d4a12fc9d0e4b87ca4c9a7291a64b55687c73558
SorsOps <80043879+sorsOps@users.noreply.github.com>
2023-09-19 10:51:48 +0200

    fix: join

--- packages/graph-engine/src/nodes/string/join.ts
+++ packages/graph-engine/src/nodes/string/join.ts
@@ -1,20 +1,24 @@
 import { NodeDefinition, NodeTypes } from "../../types.js";
 
-const type = NodeTypes.JOIN_STRING
+const type = NodeTypes.JOIN_STRING;
 
 export type NamedInput = {
-    array: string[];
-    separator: string;
+  array: string[];
+  separator: string;
 };
 
-export const process = (input: NamedInput) => {
-    const { array, separator } = input;
-    return { output: array.join(separator) };
+export const defaults = {
+  array: [],
+  separator: "",
 };
 
-
+export const process = (input: NamedInput, state: NamedInput) => {
+  const { array, separator } = { ...state, ...input };
+  return array.join(separator);
+};
 
 export const node: NodeDefinition<NamedInput> = {
-    type,
-    process,
+  defaults,
+  type,
+  process,
 };
