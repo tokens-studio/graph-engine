6326477ae247927f6d8a8ad4b36ee43f61aef5dc
SorsOps <80043879+sorsOps@users.noreply.github.com>
2023-09-20 08:56:31 +0200

    fix select

--- packages/graph-editor/src/components/flow/nodes/sets/selectToken.tsx
+++ packages/graph-editor/src/components/flow/nodes/sets/selectToken.tsx
@@ -6,43 +6,61 @@ import { PreviewAny } from '../../preview/any.tsx';
 import { WrapNode, useNode } from '../../wrapper/nodeV2.tsx';
 
 const SelectObjectPropertyNode = () => {
-    const { input, state, setState, output } = useNode();
+  const { input, state, setState, output } = useNode();
 
-    const setValue = useCallback((ev) => {
-        const value = ev.target.value;
-        const key = ev.currentTarget.dataset.key;
-        setState((state) => ({
-            ...state,
-            [key]: value,
-        }));
-    }, []);
+  const setValue = useCallback((ev) => {
+    const value = ev.target.value;
+    const key = ev.currentTarget.dataset.key;
+    setState((state) => ({
+      ...state,
+      [key]: value,
+    }));
+  }, []);
 
-    return (
-        <Stack direction="row" gap={4}>
-            <HandleContainer type="target">
-                <Handle id="object">
-                    <Text>Object</Text>
-                </Handle>
-                <Handle id="key">
-                    <Text>Key</Text>
-                    {input.key !== undefined ? (
-                        <PreviewAny value={input.key} />
-                    ) : (
-                        <TextInput onChange={setValue} value={state.key} data-key="key" />
-                    )}
-                </Handle>
-            </HandleContainer>
-            <HandleContainer type="source">
-                <Handle id="output">
-                    <Text>Output</Text>
-                    <PreviewAny value={output?.output} />
-                </Handle>
-            </HandleContainer>
-        </Stack>
-    );
+  console.log(output);
+
+  return (
+    <Stack direction="row" gap={4}>
+      <HandleContainer type="target">
+        <Handle id="object">
+          <Text>Object</Text>
+        </Handle>
+        <Handle id="key">
+          <Text>Key</Text>
+          {input.key !== undefined ? (
+            <PreviewAny value={input.key} />
+          ) : (
+            <TextInput onChange={setValue} value={state.key} data-key="key" />
+          )}
+        </Handle>
+      </HandleContainer>
+      <HandleContainer type="source">
+        <Handle id="token">
+          <Text>Token</Text>
+          <PreviewAny value={output?.token} />
+        </Handle>
+        <Handle id="name">
+          <Text>Name</Text>
+          <PreviewAny value={output?.name} />
+        </Handle>
+        <Handle id="type">
+          <Text>Type</Text>
+          <PreviewAny value={output?.type} />
+        </Handle>
+        <Handle id="value">
+          <Text>Value</Text>
+          <PreviewAny value={output?.value} />
+        </Handle>
+        <Handle id="extensions">
+          <Text>Extension</Text>
+          <PreviewAny value={output?.$extensions} />
+        </Handle>
+      </HandleContainer>
+    </Stack>
+  );
 };
 
 export default WrapNode(SelectObjectPropertyNode, {
-    ...node,
-    title: 'Select Object Property',
+  ...node,
+  title: 'Select Object Property',
 });
--- packages/graph-engine/src/nodes/set/selectToken.ts
+++ packages/graph-engine/src/nodes/set/selectToken.ts
@@ -9,33 +9,36 @@ import { NodeDefinition, NodeTypes } from "../../types.js";
 const type = NodeTypes.SELECT_TOKEN;
 
 export type Input = {
-    object: { name: string, value: any, type?: string }[];
-    key: string;
+  object: { name: string; value: any; type?: string }[];
+  key: string;
 };
 
 export type State = {
-    key: string;
+  key: string;
 };
 
 export const defaults = {
-    key: "",
+  key: "",
 };
 
 const process = (input: Input, state: State) => {
-    const final = {
-        ...state,
-        ...input,
-    };
+  const final = {
+    ...state,
+    ...input,
+  };
 
-    const foundObject = final.object.find(item => item.name === final.key);
-
-    console.log("foundObject", foundObject);
-
-    return foundObject ? foundObject : null;
+  const foundObject = final.object.find((item) => item.name === final.key);
+  return {
+    token: foundObject ? foundObject.value : null,
+    ...(foundObject || {}),
+  };
 };
 
+const mapOutput = (input, state, output) => output;
+
 export const node: NodeDefinition<Input, State> = {
-    type,
-    defaults,
-    process,
+  type,
+  defaults,
+  process,
+  mapOutput,
 };
