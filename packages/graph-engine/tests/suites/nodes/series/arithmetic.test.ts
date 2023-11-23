528d88c61e8a24d1f8046e4315b417023cf493e6
Marco Christian Krenn <marco.krenn@gmail.com>
2023-11-23 06:50:20 +0100

    add test cases for arithemtic series and precision

--- packages/graph-engine/tests/suites/nodes/series/arithmetic.test.ts
+++ packages/graph-engine/tests/suites/nodes/series/arithmetic.test.ts
@@ -9,6 +9,7 @@ describe("series/arithmetic", () => {
         stepsDown: 1,
         steps: 1,
         increment: 1,
+        precision: 0,
       },
       node,
       state: {},
@@ -20,6 +21,77 @@ describe("series/arithmetic", () => {
       0: 16,
       1: 17,
       asArray: [15, 16, 17],
+    });
+  });
+});
+
+describe("series/arithmetic with precision", () => {
+  it("handles precision correctly", async () => {
+    // Case 1: Test for precision = 1
+    const output1 = await executeNode({
+      input: {
+        base: 16,
+        stepsDown: 1,
+        steps: 1,
+        increment: 0.5,
+        precision: 1,
+      },
+      node,
+      state: {},
+      nodeId: "",
+    });
+
+    expect(output1).toStrictEqual({
+      "-1": 15.5,
+      0: 16.0,
+      1: 16.5,
+      asArray: [15.5, 16.0, 16.5],
+    });
+
+    // Case 2: Test for precision = 2
+    const output2 = await executeNode({
+      input: {
+        base: 16,
+        stepsDown: 2,
+        steps: 2,
+        increment: 0.25,
+        precision: 2,
+      },
+      node,
+      state: {},
+      nodeId: "",
+    });
+
+    expect(output2).toStrictEqual({
+      "-2": 15.5,
+      "-1": 15.75,
+      0: 16.0,
+      1: 16.25,
+      2: 16.5,
+      asArray: [15.5, 15.75, 16.0, 16.25, 16.5],
+    });
+
+    // Case 3: Test for a negative increment with precision
+    const output3 = await executeNode({
+      input: {
+        base: 16,
+        stepsDown: 2,
+        steps: 2,
+        increment: -0.3,
+        precision: 1,
+      },
+      node,
+      state: {},
+      nodeId: "",
+    });
+
+    expect(output3).toStrictEqual({
+      "-2": 16.6,
+      "-1": 16.3,
+      0: 16.0,
+      1: 15.7,
+      2: 15.4,
+      asArray: [16.6, 16.3, 16.0, 15.7, 15.4],
     });
   });
 });
