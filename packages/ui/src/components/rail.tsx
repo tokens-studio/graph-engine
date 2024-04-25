d92d48d6e7bf6f84f0549b11c4a87bd4fc4bf370
SorsOps <80043879+sorsOps@users.noreply.github.com>
2024-04-25 04:41:52 +0200

    Add next link for rail

--- packages/ui/src/components/rail.tsx
+++ packages/ui/src/components/rail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Home, GitMerge, ShoppingBag, Settings, HeadsetHelp } from 'iconoir-react';
-import { Avatar, Link, Separator, Stack } from '@tokens-studio/ui';
+import { Avatar,Box, Separator, Stack, Tooltip } from '@tokens-studio/ui';
+import Link from 'next/link'
 import TokensStudio from '@/assets/svgs/tokensstudio-logo.svg';
 
 interface RailItem {
@@ -10,40 +11,46 @@ interface RailItem {
 }
 
 const railItemsStart: RailItem[] = [
-  { icon: <Home/>, label: 'Home', link: '/' },
-  { icon: <GitMerge/>, label: 'Editor', link: '/' },
-  { icon: <ShoppingBag/>, label: 'Marketplace', link: '/' }
+  { icon: <Home />, label: 'Home', link: '/' },
+  { icon: <GitMerge />, label: 'Editor', link: '/editor' },
+  { icon: <ShoppingBag />, label: 'Marketplace', link: '/marketplace' }
 ];
 
 const railItemsEnd: RailItem[] = [
-  { icon: <Settings/>, label: 'Settings', link: '/' },
-  { icon: <HeadsetHelp/>, label: 'Help', link: '/' },
+  { icon: <Settings />, label: 'Settings', link: '/settings' },
+  { icon: <HeadsetHelp />, label: 'Help', link: '/help' },
 ];
 
 const RailItem = ({ icon, label, link }: RailItem) => {
   return (
-    <Link css={{color: '$fgOnEmphasis', padding: '$2'}} aria-label={label} href={link}>
-      {icon}
+    <Tooltip label={label}>
+      <Link aria-label={label} href={link}>
+        <Box css={{ color: '$fgOnEmphasis', padding: '$2' }}>
+        {icon}
+      </Box>
     </Link>
+     </Tooltip >
   );
 }
 
 export const Rail = () => {
   return (
-    <Stack justify='between' direction='column' css={{background: '$bgEmphasis', borderRight: '1px solid $borderSubtle', color: '$fgOnEmphasis', height: '100%', padding: '$5 $3 $3'}}>
+    <Stack justify='between' direction='column' css={{ background: '$bgEmphasis', borderRight: '1px solid $borderSubtle', color: '$fgOnEmphasis', height: '100%', padding: '$5 $3 $3' }}>
       <Stack gap={3} direction='column' align='center'>
-        <TokensStudio style={{width: 'var(--sizes-6)', height: 'auto', aspectRatio: '1/1'}} />
+        <TokensStudio style={{ width: 'var(--sizes-6)', height: 'auto', aspectRatio: '1/1' }} />
         {railItemsStart.map((item) => (
           <RailItem key={item.label} icon={item.icon} label={item.label} link={item.link} />
-          ))}
+        ))}
       </Stack>
 
       <Stack gap={3} direction='column' align='center' >
         {railItemsEnd.map((item) => (
+
           <RailItem key={item.label} icon={item.icon} label={item.label} link={item.link} />
-          ))}
-          <Separator orientation='horizontal' />
-          <Avatar src='https://xsgames.co/randomusers/assets/avatars/female/20.jpg' />
+
+        ))}
+        <Separator orientation='horizontal' />
+        <Avatar src='https://xsgames.co/randomusers/assets/avatars/female/20.jpg' />
       </Stack>
     </Stack>
   );
