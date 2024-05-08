cd63eca773b8a6eb17d8d98e11f8688a3da4bd90
SorsOps <80043879+sorsOps@users.noreply.github.com>
2024-05-08 10:34:08 +0200

    Fix non standard curve change that was breaking preview

--- packages/graph-editor/src/components/controls/curve.tsx
+++ packages/graph-editor/src/components/controls/curve.tsx
@@ -5,15 +5,19 @@ import React from 'react';
 import { Input } from '@tokens-studio/graph-engine';
 
 export const CurveField = observer(({ port, readOnly }: IField) => {
-  const [forceRender, setForceRender] = React.useState(0);
   const onChange = (index: number, value: number[]) => {
     if (!readOnly) {
-      port.value.curves[0].points[index] = value;
-      (port as Input).setValue(port.value);
+      const points = [...port.value.curves[0].points];
+      points[index] = value;
+      (port as Input).setValue({
+        curves: [
+          {
+            points,
+          },
+        ],
+      });
     }
-    setForceRender(forceRender + 1);
   };
-
   return (
     <CurveEditor points={port.value.curves[0].points} onChange={onChange} />
   );
