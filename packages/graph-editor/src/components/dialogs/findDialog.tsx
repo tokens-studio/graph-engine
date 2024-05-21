997db1c73322b3befd3e99d13a9979ce97f24c38
SorsOps <80043879+sorsOps@users.noreply.github.com>
2024-05-21 17:50:30 +0200

    Fix finding nodes

--- packages/graph-editor/src/components/dialogs/findDialog.tsx
+++ packages/graph-editor/src/components/dialogs/findDialog.tsx
@@ -1,22 +1,90 @@
 import React from 'react';
-import { Button, Dialog, TextInput, Text } from '@tokens-studio/ui';
+import { Button, Dialog, TextInput, Text, IconButton } from '@tokens-studio/ui';
+import { useSelector } from 'react-redux';
+import { graphEditorSelector } from '@/redux/selectors';
+import { Xmark } from 'iconoir-react';
+import { useGraph } from '@/hooks';
+import { title as annotatedTitle } from '@/annotations';
 
 export const FindDialog = ({ children }) => {
   const [id, setId] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const localGraph = useGraph();
+  const graph = useSelector(graphEditorSelector);
+  const [open, setOpen] = React.useState(false);
 
-  const onClick = () => {};
+  const onClick = () => {
+    const reactflow = graph?.getFlow();
+    if (!localGraph || !reactflow) {
+      return
+    }
+
+
+
+
+    const node = reactflow.getNodes().find((n) => n.id === id);
+
+    if (node) {
+      reactflow.fitView({ padding: 0.2, includeHiddenNodes: true, nodes: [node] });
+      setOpen(false);
+    }
+
+  };
+
+  const onClickTitle = () => {
+    const reactflow = graph?.getFlow();
+    if (!localGraph || !reactflow) {
+      return
+    }
+
+
+    const graphNodes = Object.values(localGraph.nodes);
+
+
+    const graphNode = graphNodes.find((n) => n.annotations[annotatedTitle] === title);
+    if (!graphNode) {
+      return;
+    }
+    const flowNode = reactflow.getNode(graphNode?.id);
+    if (flowNode) {
+      reactflow.fitView({ padding: 0.2, includeHiddenNodes: true, nodes: [flowNode] });
+      setOpen(false);
+    }
+
+  }
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>{children}</Dialog.Trigger>
-      <Dialog.SimplePortal>
+      <Dialog.Portal>
+        <Dialog.Overlay />
+        <Dialog.Content>
           <Dialog.Title>Find Node</Dialog.Title>
 
           <Text>Find by ID</Text>
           <TextInput value={id} onChange={(e) => setId(e.target.value)} />
           <Button onClick={onClick}>Find</Button>
 
-      </Dialog.SimplePortal>
+          <Text>Find by Title</Text>
+          <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+          <Button onClick={onClickTitle}>Find</Button>
+
+
+
+          <IconButton
+            css={{
+              position: "absolute",
+              top: "$4",
+              right: "$4"
+            }}
+            onClick={() => setOpen(false)}
+            aria-label="Close"
+            variant="invisible"
+            icon={<Xmark />}
+          />
+        </Dialog.Content>
+      </Dialog.Portal>
+
     </Dialog>
   );
 };
