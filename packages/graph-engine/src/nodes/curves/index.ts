addb50daac84421673a78b469835961b3af2c86b
Marco Christian Krenn <marco.krenn@gmail.com>
2024-06-27 13:19:54 +0200

    Add bezier curve (#375)
    * add bezier curve
    
    refs #353
    
    * Update
    
    * Format
    
    ---------
    
    Co-authored-by: SorsOps <80043879+sorsOps@users.noreply.github.com>

--- packages/graph-engine/src/nodes/curves/index.ts
+++ packages/graph-engine/src/nodes/curves/index.ts
@@ -1,3 +1,5 @@
+import bezier from "./bezier.js";
+import presetBezier from './presetBeziers.js';
 import sample from "./sample.js";
 
-export const nodes = [sample];
+export const nodes = [bezier, presetBezier, sample];
