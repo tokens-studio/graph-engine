1653781ee69a7e969f77cddf9e8e9a0b1a2f814b
SorsOps <80043879+sorsOps@users.noreply.github.com>
2024-07-02 14:44:00 +0200

    Cleanup graph

--- packages/ui/src/redux/store.tsx
+++ packages/ui/src/redux/store.tsx
@@ -1,56 +1,16 @@
+import { JourneyState } from './models/journey.ts';
+import { RefState } from './models/refs.ts';
 import { RematchDispatch, init } from '@rematch/core';
 import { RootModel, models } from './models/index.ts';
 import { UIState } from './models/ui.ts';
 import persistPlugin from '@rematch/persist';
 import storage from 'redux-persist/lib/storage';
-import { JourneyState } from './models/journey.ts';
-import { RefState } from './models/refs.ts';
-import { createTransform } from 'redux-persist';
-import { StorageState, storageState } from './models/storage.ts';
-import { GithubStorage } from '@/lib/storage/providers/github.ts';
-
-
-const StorageTransform = createTransform(
-  // transform state on its way to being serialized and persisted.
-  (inboundState: object, key) => {
-    if (key === 'storage') {
-      const inbound: StorageState = inboundState as StorageState;
-
-      if (inbound.storage) {
-        return {
-          ...inbound, storage: inbound.storage.serialize(),
-        }
-      }
-
-    }
-    // convert mySet to an Array.
-    return { ...inboundState };
-  },
-  // transform state being rehydrated
-  (outboundState, key) => {
-    if (key === 'storage') {
-      const outbound: StorageState = outboundState as StorageState;
-
-      //TODO this is hardcoded
-      const val = {
-        ...storageState.state,
-        ...outbound,
-        storage: outbound.storage ? GithubStorage.deserialize(outbound.storage) : undefined
-      };
-      return val;
-
-    }
-    // convert mySet back to a Set.
-    return { ...outboundState };
-  }
-);
-
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['journey', 'settings', 'storage'],
-  transforms: [StorageTransform],
+  transforms: [],
 };
 
 
@@ -71,5 +31,4 @@ export type RootState = {
   ui: UIState;
   journey: JourneyState;
   refs: RefState;
-  storage: StorageState;
 };
