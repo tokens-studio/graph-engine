f3af3c70e627efc53c9d69b5383acae9343b4033
SorsOps <80043879+sorsOps@users.noreply.github.com>
2024-07-10 14:09:05 +0200

    Fix base path

--- packages/ui/src/api/sdk/index.ts
+++ packages/ui/src/api/sdk/index.ts
@@ -2,6 +2,6 @@ import { contract } from '../contracts/index.ts';
 import { initQueryClient } from '@ts-rest/react-query';
 
 export const client = initQueryClient(contract, {
-	baseUrl: 'http://localhost:3000/api/v1',
+	baseUrl: '/api/v1',
 	baseHeaders: {}
 });
