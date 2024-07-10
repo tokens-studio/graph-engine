43c99a9c8705b6577110f2678d99280a53e60e99
SorsOps <80043879+sorsOps@users.noreply.github.com>
2024-07-10 10:54:14 +0200

    Add console log to debug auth callbacks in prod

--- packages/ui/src/auth/index.ts
+++ packages/ui/src/auth/index.ts
@@ -2,12 +2,25 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import GitHub from 'next-auth/providers/github';
 import NextAuth from 'next-auth';
 import { prisma } from '@/lib/prisma/index.ts';
+import { headers } from 'next/headers.js';
 
 
 const adapter = NextAuth({
+
 	adapter: PrismaAdapter(prisma),
 	providers: [GitHub],
-	trustHost: true
+	trustHost: true,
+	callbacks: {
+		async redirect({ url, baseUrl }) {
+			//Debug
+			console.log(url,baseUrl,headers().get('host'))
+			// Allows relative callback URLs
+			if (url.startsWith("/")) return `${baseUrl}${url}`
+			// Allows callback URLs on the same origin
+			else if (new URL(url).origin === baseUrl) return url
+			return baseUrl
+		}
+	},
 });
 
 export const { handlers, auth, signIn, signOut } = adapter;
