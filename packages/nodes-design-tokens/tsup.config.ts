f83091a2d0268307869d77d50e460dcf98e31e62
SorsOps <80043879+SorsOps@users.noreply.github.com>
2024-07-28 10:29:43 +0200

    Cleanup. Add cypress id (#459)
    * Cleanup. Add cypress id
    
    * Format

--- packages/nodes-design-tokens/tsup.config.ts
+++ packages/nodes-design-tokens/tsup.config.ts
@@ -4,11 +4,11 @@ const env: string = process.env.NODE_ENV || 'development';
 
 export const tsup: Options = {
 	sourcemap: env === 'production', // source map is only available in prod
-	clean: false,
+	clean: false, // rimraf dist
 	dts: true, // generate dts file for main module
 	format: 'esm',
 	outDir: 'dist',
-	entry: ['src/**/*.ts', 'src/**/*.tsx'],
+	entry: ['src/index.ts'],
 	minify: false,
 	target: 'esnext'
 };
