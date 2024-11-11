ba4e0afaaade25ca63e345b48242f35a1ad4444e
Marco Christian Krenn <marco.krenn@gmail.com>
2024-11-11 12:59:21 +0100

    fix power series

--- packages/graph-engine/src/nodes/series/power.ts
+++ packages/graph-engine/src/nodes/series/power.ts
@@ -4,11 +4,6 @@ import { NumberSchema } from '../../schemas/index.js';
 import { arrayOf } from '../../schemas/utils.js';
 import { setToPrecision } from '../../utils/precision.js';
 
-type PowerValue = {
-	index: number;
-	value: number;
-};
-
 export default class NodeDefinition extends Node {
 	static title = 'Power Series';
 	static type = 'studio.tokens.series.power';
@@ -52,11 +47,10 @@ export default class NodeDefinition extends Node {
 
 	execute(): void | Promise<void> {
 		const { base, powers, precision } = this.getAllInputs();
-		const values: PowerValue[] = powers.map((power, index) => ({
-			index,
-			value: setToPrecision(Math.pow(base, power), precision)
-		}));
+		const values: number[] = powers.map(power =>
+			setToPrecision(Math.pow(base, power), precision)
+		);
 
-		this.outputs.array.set(values.map(x => x.value));
+		this.outputs.array.set(values);
 	}
 }
