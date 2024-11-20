f5c0404afa749ba81844eeefef9b290887c7e756
AlexBxl <alex@hyma.io>
2024-11-20 17:18:03 +0100

    added isRange and color outputs to Match Alpha node
    fixed incorrect precision test
    updated tests for new outputs
    added Gray color preset to utils

--- packages/graph-engine/src/nodes/color/matchAlpha.ts
+++ packages/graph-engine/src/nodes/color/matchAlpha.ts
@@ -1,8 +1,13 @@
-import { ColorSchema, NumberSchema } from '../../schemas/index.js';
+import { Black, Gray, White, toColor, toColorObject } from './lib/utils.js';
+import {
+	BooleanSchema,
+	ColorSchema,
+	NumberSchema
+} from '../../schemas/index.js';
 import { INodeDefinition } from '../../index.js';
 import { Node } from '../../programmatic/node.js';
 import { setToPrecision } from '@/utils/precision.js';
-import { toColor } from './lib/utils.js';
+import Color from 'colorjs.io';
 
 export default class NodeDefinition extends Node {
 	static title = 'Match Alpha';
@@ -13,13 +18,22 @@ export default class NodeDefinition extends Node {
 		super(props);
 
 		this.addInput('foreground', {
-			type: ColorSchema
+			type: {
+				...ColorSchema,
+				default: Black
+			}
 		});
 		this.addInput('background', {
-			type: ColorSchema
+			type: {
+				...ColorSchema,
+				default: White
+			}
 		});
 		this.addInput('reference', {
-			type: ColorSchema
+			type: {
+				...ColorSchema,
+				default: Gray
+			}
 		});
 		this.addInput('precision', {
 			type: {
@@ -28,6 +42,12 @@ export default class NodeDefinition extends Node {
 			}
 		});
 
+		this.addOutput('inRange', {
+			type: BooleanSchema
+		});
+		this.addOutput('color', {
+			type: ColorSchema
+		});
 		this.addOutput('alpha', {
 			type: NumberSchema
 		});
@@ -42,6 +62,7 @@ export default class NodeDefinition extends Node {
 		const ref = toColor(reference);
 
 		let alpha = Number.NaN;
+		let inRange = false;
 
 		// the matching is done per channel
 
@@ -52,6 +73,7 @@ export default class NodeDefinition extends Node {
 			Math.abs(bg.b - ref.b) < precision
 		) {
 			alpha = 0;
+			inRange = true;
 		} else {
 			// find the matching alpha for each channel
 			const ar = validateAlpha((ref.r - bg.r) / (fg.r - bg.r));
@@ -61,26 +83,37 @@ export default class NodeDefinition extends Node {
 			// return the average of the alphas for all matched channels
 			if (!isNaN(ar) && !isNaN(ag) && !isNaN(ab)) {
 				if (
-					Math.abs(ar - ag) < precision &&
-					Math.abs(ag - ab) < precision &&
-					Math.abs(ar - ab) < precision
+					Math.abs(ar - ar) < precision &&
+					Math.abs(ag - ag) < precision &&
+					Math.abs(ab - ab) < precision
 				) {
 					alpha = (ar + ag + ab) / 3;
+					inRange = true;
 				}
 			} else if (!isNaN(ar) && !isNaN(ag)) {
-				if (Math.abs(ar - ag) < precision) {
+				if (
+					Math.abs(ar - ag) < precision
+				) {
 					alpha = (ar + ag) / 2;
+					inRange = true;
 				}
 			} else if (!isNaN(ag) && !isNaN(ab)) {
-				if (Math.abs(ag - ab) < precision) {
+				if (
+					Math.abs(ag - ab) < precision
+				) {
 					alpha = (ag + ab) / 2;
+					inRange = true;
 				}
 			} else if (!isNaN(ar) && !isNaN(ab)) {
-				if (Math.abs(ar - ab) < precision) {
+				if (
+					Math.abs(ar - ab) < precision
+				) {
 					alpha = (ar + ab) / 2;
+					inRange = true;
 				}
 			} else {
 				alpha = ar || ag || ab;
+				inRange = !isNaN(alpha);
 			}
 		}
 
@@ -90,11 +123,44 @@ export default class NodeDefinition extends Node {
 			Math.max(0, -Math.floor(Math.log10(precision)))
 		);
 
+
+		// calculate the composite color, and if it's too far from the reference, return NaN
+		// deltaE() returns values typically ranging from 0 to 100, so I divide it by 100
+		// to compare normalized colors
+		const comp = blendColors(fg, bg, alpha);
+
+		if (comp.deltaE2000(ref) / 100 > precision)
+		{
+			alpha = Number.NaN;
+			inRange = false;
+		}
+
+
+		// if the resulting alpha is valid, assign it to the foreground color,
+		// which becomes the output color
+		if (inRange) fg.alpha = alpha;
+
+
+		this.outputs.inRange.set(inRange);
+		this.outputs.color.set(toColorObject(fg));
 		this.outputs.alpha.set(alpha);
 	}
 }
 
-function validateAlpha(alpha: number): number {
+function validateAlpha(alpha: number) {
 	// set valid but out-of-range alphas to NaN
 	return !isNaN(alpha) && alpha >= 0 && alpha <= 1 ? alpha : Number.NaN;
+}
+
+function blendChannels(fg: number, bg: number, alpha: number)
+{
+	return fg * alpha + bg * (1 - alpha);
+}
+
+function blendColors(fg: Color, bg: Color, alpha: number)
+{
+	return new Color('srgb', [
+		blendChannels(fg.r, bg.r, alpha),
+		blendChannels(fg.g, bg.g, alpha),
+		blendChannels(fg.b, bg.b, alpha)]);
 }
