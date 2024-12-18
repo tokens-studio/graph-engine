b4058afbfd85f54418f3c44a64121f68133d8a70
Florian Schroedl <flo.schroedl@gmail.com>
2024-12-18 17:58:55 +0100

    Extract callback

--- packages/graph-editor/src/components/panels/navigator/index.tsx
+++ packages/graph-editor/src/components/panels/navigator/index.tsx
@@ -1,75 +1,10 @@
-import { Label, Stack, Text, TextInput, Textarea } from '@tokens-studio/ui';
-import React, { useCallback, useMemo } from 'react';
+import { Stack } from '@tokens-studio/ui';
+import React from 'react';
 
-import { ErrorBoundary } from 'react-error-boundary';
-import { GraphEditor } from '@/editor/graphEditor.js';
-import { ErrorBoundaryContent } from '@/components/ErrorBoundaryContent.js';
-import { Node } from '@tokens-studio/graph-engine';
-import {
-  currentNode,
-  graphNodesSelector,
-  currentPanelIdSelector,
-} from '@/redux/selectors/graph.js';
-import { dockerSelector } from '@/redux/selectors/refs.js';
-import { description, title as annotatedTitle } from '@/annotations/index.js';
-import { observer } from 'mobx-react-lite';
-import { useGraph } from '@/hooks/useGraph.js';
+import { graphNodesSelector } from '@/redux/selectors/graph.js';
 import { useDispatch } from '@/hooks/useDispatch.js';
 import { useSelector } from 'react-redux';
-
-const switchOrCreateGraphTab = function (node) {
-  const graphId = innerGraph.annotations['engine.id'];
-  const title =
-    node.annotations[annotatedTitle] ||
-    innerGraph.annotations['engine.title'] ||
-    'Subgraph';
-  const existing = dockerRef.current.find(graphId);
-};
-
-const useSubgraphExplorerCallback = (node) => {
-  const dockerRef = useSelector(dockerSelector);
-  const callback = useCallback(() => {
-    if (!dockerRef?.current) {
-      return;
-    }
-
-    let oneShot = false;
-    const innerGraph = node._innerGraph;
-    const graphId = innerGraph.annotations['engine.id'];
-    const title =
-      node.annotations[annotatedTitle] ||
-      innerGraph.annotations['engine.title'] ||
-      'Subgraph';
-    const existing = dockerRef.current.find(graphId);
-
-    const ref = (o: ImperativeEditorRef) => {
-      if (o && !oneShot) {
-        o.load(innerGraph);
-        oneShot = true;
-      }
-    };
-
-    if (!existing) {
-      const newTab = {
-        cached: true,
-        closable: true,
-        id: graphId,
-        group: 'graph',
-        title,
-        content: (
-          <ErrorBoundary fallback={<ErrorBoundaryContent />}>
-            <GraphEditor ref={ref} id={graphId} />
-          </ErrorBoundary>
-        ),
-      };
-      dockerRef.current.dockMove(newTab, 'graphs', 'middle');
-    } else {
-      dockerRef.current.updateTab(graphId, null, true);
-    }
-  }, [dockerRef, node._innerGraph, node.annotations]);
-
-  return callback;
-};
+import { useSubgraphExplorerCallback } from '@/registry/specifics.js';
 
 const SubgraphNodeItem = function ({ node }) {
   const nodeType = node.nodeType();
