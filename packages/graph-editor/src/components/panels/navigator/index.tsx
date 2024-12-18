ab8a1a5293900c6229ef184bb4365f2833acaad7
Florian Schroedl <flo.schroedl@gmail.com>
2024-12-18 17:39:33 +0100

    Make subgraphs navigateable

--- packages/graph-editor/src/components/panels/navigator/index.tsx
+++ packages/graph-editor/src/components/panels/navigator/index.tsx
@@ -1,18 +1,90 @@
 import { Label, Stack, Text, TextInput, Textarea } from '@tokens-studio/ui';
 import React, { useCallback, useMemo } from 'react';
 
+import { ErrorBoundary } from 'react-error-boundary';
+import { GraphEditor } from '@/editor/graphEditor.js';
+import { ErrorBoundaryContent } from '@/components/ErrorBoundaryContent.js';
 import { Node } from '@tokens-studio/graph-engine';
-import { currentNode, graphNodesSelector } from '@/redux/selectors/graph.js';
-import { description, title } from '@/annotations/index.js';
+import {
+  currentNode,
+  graphNodesSelector,
+  currentPanelIdSelector,
+} from '@/redux/selectors/graph.js';
+import { dockerSelector } from '@/redux/selectors/refs.js';
+import { description, title as annotatedTitle } from '@/annotations/index.js';
 import { observer } from 'mobx-react-lite';
 import { useGraph } from '@/hooks/useGraph.js';
+import { useDispatch } from '@/hooks/useDispatch.js';
 import { useSelector } from 'react-redux';
 
-export const Navigator = () => {
-  const graph = useGraph();
-  const nodes = useSelector(graphNodesSelector);
+const switchOrCreateGraphTab = function (node) {
+  const graphId = innerGraph.annotations['engine.id'];
+  const title =
+    node.annotations[annotatedTitle] ||
+    innerGraph.annotations['engine.title'] ||
+    'Subgraph';
+  const existing = dockerRef.current.find(graphId);
+};
 
-  console.log(nodes);
+const useSubgraphExplorerCallback = (node) => {
+  const dockerRef = useSelector(dockerSelector);
+  const callback = useCallback(() => {
+    if (!dockerRef?.current) {
+      return;
+    }
+
+    let oneShot = false;
+    const innerGraph = node._innerGraph;
+    const graphId = innerGraph.annotations['engine.id'];
+    const title =
+      node.annotations[annotatedTitle] ||
+      innerGraph.annotations['engine.title'] ||
+      'Subgraph';
+    const existing = dockerRef.current.find(graphId);
+
+    const ref = (o: ImperativeEditorRef) => {
+      if (o && !oneShot) {
+        o.load(innerGraph);
+        oneShot = true;
+      }
+    };
+
+    if (!existing) {
+      const newTab = {
+        cached: true,
+        closable: true,
+        id: graphId,
+        group: 'graph',
+        title,
+        content: (
+          <ErrorBoundary fallback={<ErrorBoundaryContent />}>
+            <GraphEditor ref={ref} id={graphId} />
+          </ErrorBoundary>
+        ),
+      };
+      dockerRef.current.dockMove(newTab, 'graphs', 'middle');
+    } else {
+      dockerRef.current.updateTab(graphId, null, true);
+    }
+  }, [dockerRef, node._innerGraph, node.annotations]);
+
+  return callback;
+};
+
+const SubgraphNodeItem = function ({ node }) {
+  const nodeType = node.nodeType();
+  const onNodeClick = useSubgraphExplorerCallback(node);
+
+  return (
+    <li key={node.id} onClick={onNodeClick}>
+      <span>{nodeType}</span>
+    </li>
+  );
+};
+
+export const Navigator = () => {
+  const nodes = useSelector(graphNodesSelector);
+  const dispatch = useDispatch();
 
   return (
     <Stack
@@ -25,7 +97,14 @@ export const Navigator = () => {
         overflow: 'auto',
       }}
     >
-      <div style={{ padding: 'var(--component-spacing-md)' }}>Yaw</div>
+      <div style={{ padding: 'var(--component-spacing-md)' }}>
+        <ul>
+          {Object.values(nodes).map((node) => {
+            if (!node._innerGraph) return null;
+            return <SubgraphNodeItem key={node.id} node={node} />;
+          })}
+        </ul>
+      </div>
     </Stack>
   );
 };
