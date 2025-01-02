ae83d5089a09015c819cca31eb4192eeee75552e
Marco Christian Krenn <marco.krenn@gmail.com>
2025-01-02 22:24:00 +0100

    add tokens table

--- packages/nodes-previews/src/index.ts
+++ packages/nodes-previews/src/index.ts
@@ -3,16 +3,21 @@ import { ColorSwatch } from './color/swatch/specific.js';
 
 import { DimensionScale } from './dimension/scale/specific.js';
 
+import { TokenTable } from './token/table/specific.js';
+
 import ColorScaleNode from './color/scale/node.js';
 import ColorSwatchNode from './color/swatch/node.js';
 import DimensionScaleNode from './dimension/scale/node.js';
+
+import TokenTableNode from './token/table/node.js';
 
 import type { Node } from '@tokens-studio/graph-engine';
 
 export const specifics = {
 	'studio.tokens.previews.color.scale': ColorScale,
 	'studio.tokens.previews.color.swatch': ColorSwatch,
-	'studio.tokens.previews.dimension.scale': DimensionScale
+	'studio.tokens.previews.dimension.scale': DimensionScale,
+	'studio.tokens.previews.tokens.table': TokenTable
 };
 
 /**
@@ -21,5 +26,6 @@ export const specifics = {
 export const nodes: (typeof Node)[] = ([] as (typeof Node)[]).concat(
 	ColorScaleNode,
 	ColorSwatchNode,
-	DimensionScaleNode
+	DimensionScaleNode,
+	TokenTableNode
 );
