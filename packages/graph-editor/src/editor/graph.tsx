ae50f8bca21560851cbc6fc8b41651321d491593
AlexBxl <alex@hyma.io>
2025-01-15 10:47:03 +0100

    moved CSS to modules file

--- packages/graph-editor/src/editor/graph.tsx
+++ packages/graph-editor/src/editor/graph.tsx
@@ -36,6 +36,7 @@ import React, {
   useState,
 } from 'react';
 import ReactFlow from 'reactflow';
+import toolbarStyles from '../components/toolbar/toolbar.module.css';
 
 import groupNode from '../components/flow/nodes/groupNode.js';
 import noteNode from '../components/flow/nodes/noteNode.js';
@@ -812,18 +813,7 @@ export const EditorApp = React.forwardRef<
                   handleSelectNewNodeType={handleSelectNewNodeType}
                 />
               )}
-              <div
-                style={{
-                  position: 'absolute',
-                  top: 'var(--size-250)',
-                  left: 0,
-                  right: 0,
-                  display: 'grid',
-                  placeItems: 'center',
-                  zIndex: '99',
-                  pointerEvents: 'none',
-                }}
-              >
+              <div className={toolbarStyles.wrapper}>
                 <GraphToolbar />
               </div>
               {props.children}
